ddcd108933d79d3a980f8276a6a1210f42dccd60
Robert Le <robertle982@gmail.com>
2024-12-16 21:29:23 -0500

    fixed PlacesToVisit.jsx

--- TripPlanner.API/obj/Debug/net9.0/TripPlanner.API.AssemblyInfo.cs
+++ TripPlanner.API/obj/Debug/net9.0/TripPlanner.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TripPlanner.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931765be8e7a5a63b3fed806d97845dddaccde29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537d1fcf0035162ba2f46cac1db2d78c4a47c3de")]
 [assembly: System.Reflection.AssemblyProductAttribute("TripPlanner.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TripPlanner.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/view-trip/components/PlacesToVisit.jsx
+++ frontend/src/view-trip/components/PlacesToVisit.jsx
@@ -7,12 +7,12 @@ function PlacesToVisit({trip}) {
         <h2 className='font-bold text-lg'>Places to Visit</h2>
     
         <div>
-            {trip.tripData?.itinerary.map((item,index)=>(
-                <div className='mt-5'>
+            {trip?.tripData?.itinerary?.map((item,index)=>(
+                <div className='mt-5' key={index}>
                     <h2 className='font-medium text-lg'>{item.day}</h2>
                     <div className='grid md:grid-cols-2 gap-5'>
-                    {item.plan.map((place,index)=>(
-                            <div className=''>
+                    {item.plan?.map((place,index)=>(
+                            <div key={index}>
                                 <h2 className='font-medium text-sm text-orange-600'>{place.time}</h2>
                                 <PlaceCardItem place={place} />
                             </div>
